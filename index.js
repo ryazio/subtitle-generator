b6421418f2e1b9c6576ba875827fc894d5850e99
Gauravjeet Singh <gaurav.ishwerdas@gmail.com>
2023-02-03 21:50:26 +0530

    fix: use seconds value from jsonData to generate hours, minutes and milliseconds

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { generate } = require("./lib/srtGenerator");
 
-exports.generateSRT = (data, format, startTime='00:00:00:00') => {
-	try {
-		return generate(data, format, startTime);
-	} catch (error) {
-		console.error(error);
-	}
-}
+exports.generateSRT = (data, format, startTime = "00:00:00:00") => {
+  try {
+    return generate(data, format, startTime);
+  } catch (error) {
+    console.error(error);
+  }
+};
